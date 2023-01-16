789abe2e34884b05bcebfd7b29cd7061d16f341f
Cissé <tairou.cisse@axazara.com>
2023-01-16 14:35:16 +0100

    - implement navbar hero pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function HeadlineCards() {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      {/* Card */}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div>
+          <p>Sun's Out, BOGO's Out</p>
+          <p>Throug 8/26</p>
+          <button className="border border-black rounded-xl px-5 py-1">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+}
